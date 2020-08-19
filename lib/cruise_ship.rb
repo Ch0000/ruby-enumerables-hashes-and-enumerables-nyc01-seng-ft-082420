43050fd7420f4b88c5b4aad00a0b79476a98e9ac
Christian P Andrews <cpandrews91@gmail.com>
2020-08-19 01:52:24 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,18 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+}
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+  if suite == :suite_a && name.start_With?("A")
+    winner = name
+  end
 end
+
+winner
+#select_winner returns the name of the passenger who stays in suite a and whose name begins with the letter 'A'
